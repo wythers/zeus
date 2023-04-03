68f7c5fba8b80f1c18edea05b09b4b6771512929
Wyther-Yang <958700433@qq.com>
2023-04-03 15:09:32 +0800

    init push

---
+++ cache_test.cpp
@@ -0,0 +1,179 @@
+#pragma once
+// Copyright (C) 2021- 
+
+#define ZEUS_LIBRARY_DEBUG
+#include "cache.hpp"
+
+#include <iostream>
+
+#define PASSED 0
+#define N  10 << 10
+
+using namespace zeus;
+using namespace std;
+
+
+
+using Case = int (*)();
+
+/**
+ * zeus::pool unit test.
+*/
+/*
+int mainn() {
+        Case    cases[4]{
+                        test_cache_case1,
+                        test_cache_case2,
+                        test_cache_case3,
+                        test_cache_case4
+                };
+
+        char const* hdr = "zeus::pooll test case #";
+        for (int i = 0; i < 4; i++) {
+                int ret = cases[i]();
+                printf( "=== RUN %s%d "
+                        "--------------"
+                        "-------------- %s\n", 
+                        hdr, i+1,
+                        ret == PASSED ? "passed" : "fail");
+        }
+
+        return 0;
+}
+*/
+
+
+
+struct tester {
+        atomic_ulong& cnt;
+        tester(atomic<ulong>& c) : cnt(c)  {
+                cnt++;
+        }
+        ~tester() {
+                cnt--;
+        }
+};
+//zeus::Pool<tester, 8> pool{};
+// construction/destruction test
+int test_cache_case1(auto& pool) {
+//        zeus::Pool<tester, 8> pool{};
+        atomic_ulong cnt = 0;
+        vector<thread> ths{};
+        atomic_bool flag = false;
+        auto [Vcnt, Ccnt] = decay_t<decltype(pool)>::ZeusUnitMonitor();
+
+        for (uint i = 0; i < 8; i++) {
+                ths.emplace_back(thread([&]{
+                        auto* p = pool.Get(cnt);
+                        flag.wait(false);
+                        pool.Put(p);
+                }));
+        }
+
+        flag = true;
+        flag.notify_all();
+        for (auto& th : ths) {
+                th.join();
+        }
+
+        return cnt.load() || Vcnt.load() || Ccnt.load();
+}
+
+// one to one
+int test_cache_case2() {
+        zeus::Pool<tester, 8> pool{};
+        auto [Vcnt, Ccnt] = decltype(pool)::ZeusUnitMonitor();
+        atomic_ulong cnt{}; 
+        atomic_bool flag{};  
+
+        thread th1([&]{
+                for (ulong i = 0; i < N; i++) {
+                        pool.Put(new tester{cnt});
+                }
+                flag = true;
+        });
+        
+        for (;;) {
+                auto* p = pool.Get(cnt);
+                if (!p && !flag)
+                        continue;
+                if (p) {
+                        delete p;
+                } else {
+                        break;
+                }
+        }
+
+        th1.join();
+        return cnt.load() || Vcnt.load() || Ccnt.load();
+}
+
+// many to one
+int test_cache_case3() {
+        zeus::Pool<tester, 8> pool{};
+        atomic_ulong cnt = 0;
+        atomic_uint cnt2 = 0;
+        vector<thread> ths{};
+        
+        auto [Vcnt, Ccnt] = decltype(pool)::ZeusUnitMonitor();
+
+        for (uint i = 0; i < 4; i++) {
+                ths.emplace_back(thread([&]{
+                        for (ulong j = 0; j < N / 4; j++) {
+                                pool.Put(new tester(cnt));
+                        }
+                        cnt2++;
+                }));
+        }
+
+        for (;;) {
+                auto* p = pool.Get(cnt);
+                if (p) 
+                        delete p;
+                if (cnt2 == 4)
+                        break;
+        }
+
+        for (auto& th : ths) {
+                th.join();
+        }
+
+        return cnt.load() || Vcnt.load() || Ccnt.load();
+}
+
+// many to many
+int test_cache_case4() {
+        zeus::Pool<tester, 16> pool{};
+        atomic_ulong cnt = 0;
+        atomic_uint cnt2 = 0;
+        vector<thread> ths{};
+        
+        auto [Vcnt, Ccnt] = decltype(pool)::ZeusUnitMonitor();
+
+        for (uint i = 0; i < 8; i++) {
+                ths.emplace_back(thread([&]{
+                        for (ulong j = 0; j < N / 8; j++) {
+                                pool.Put(new tester(cnt));
+                        }
+                        cnt2++;
+                }));
+        }
+
+        for (uint i = 0; i < 8; i++) {
+                ths.emplace_back(thread([&]{
+                        for (;;) {
+                                auto* p = pool.Get(cnt);
+                                if (p) 
+                                        delete p;
+                                if (cnt2 == 8)
+                                        break;
+                        }
+                }));
+        }
+
+        for (auto& th : ths) {
+                th.join();
+        }
+
+        return cnt.load() || Vcnt.load() || Ccnt.load();
+}
